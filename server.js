c6d4e93c44d6408b6b368e40f0f21bdb173ef79e
Sergey Razin <900556@mail.ru>
2020-01-19 16:31:11 +0400

    prepare to deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
+const path = require('path');
 
 // подклюение к монго
 connectDB();
@@ -8,6 +9,14 @@ connectDB();
 app.use(express.json({ limit: '10mb', extended: false }));
 
 app.use(require('./routes'));
+
+// Подключаем статические файлы для продакшена
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
